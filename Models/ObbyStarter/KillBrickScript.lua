612f4b93a42e55f217a855eaa7303b4ff66814c9
jacobhumston <57332486+jacobhumston@users.noreply.github.com>
2021-03-16 15:08:02 -0400

    Create KillBrickScript.lua

---
+++ Models/ObbyStarter/KillBrickScript.lua
@@ -0,0 +1,11 @@
+cooldown = false
+script.Parent.Touched:Connect(function(touched)
+	if touched.Parent:FindFirstChild("Humanoid") then
+		if cooldown == false then
+			cooldown = true
+			touched.Parent.Humanoid:TakeDamage(script.Damage.Value)
+			wait(script.Cooldown.Value)
+			cooldown = false
+		end
+	end
+end)
