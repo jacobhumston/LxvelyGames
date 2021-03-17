31d5eca220911282df7268d07ff99ffc6c642bda
jacobhumston <57332486+jacobhumston@users.noreply.github.com>
2021-03-17 15:58:21 -0400

    Update RandomFunctions.lua

--- Extras/RandomFunctions.lua
+++ Extras/RandomFunctions.lua
@@ -1,8 +1,8 @@
 function KillWorkspace()
-  for i,v in ipairs(workspace:GetChildren()) do
-    if v ~= script then
-    v:Destroy()
-    end
-  end
-  print("haha workspace went boom")
+	for i,v in ipairs(workspace:GetChildren()) do
+		if v ~= script and v:IsA("Terrain") == false and v:IsA("Camera") == false then
+			v:Destroy()
+		end
+	end
+	print("haha workspace went boom")
 end
