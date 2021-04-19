f14bb91a39a1a1507b6f6cf3d57b2a9adc743c84
jacobhumston <57332486+jacobhumston@users.noreply.github.com>
2021-04-19 19:50:59 -0400

    Create HandyFunctions.lua

---
+++ Extras/HandyFunctions.lua
@@ -0,0 +1,51 @@
+local HandyFunctions = {}
+
+function HandyFunctions.ClearTypeOfChildren(instance,objecttype)
+	for index, value in ipairs(instance:GetChildren()) do
+		if value:IsA(objecttype) then
+			value:Destroy()
+		end
+	end	
+end
+
+function HandyFunctions.ClearTypeOfDescendant(instance,objecttype)
+	for index, value in ipairs(instance:GetDescendants()) do
+		if value:IsA(objecttype) then
+			value:Destroy()
+		end
+	end	
+end
+
+function HandyFunctions.ReturnListOfChildren(instance,printindex,newline) 
+	local tableofchildren = {}
+	for index, value in ipairs(instance:GetChildren()) do
+		if printindex == true then
+			table.insert(tableofchildren,#tableofchildren+1,"["..index.."] "..value.Name)	
+		elseif printindex == false then
+			table.insert(tableofchildren,#tableofchildren+1,value.Name)
+		end
+	end
+	if newline == true then
+		return table.concat(tableofchildren,"\n")
+	elseif newline == false then
+		return table.concat(tableofchildren,", ")
+	end
+end
+
+function HandyFunctions.ReturnListOfDescendants(instance,printindex,newline) 
+	local tableofchildren = {}
+	for index, value in ipairs(instance:GetDescendants()) do
+		if printindex == true then
+			table.insert(tableofchildren,#tableofchildren+1,"["..index.."] "..value.Name)	
+		elseif printindex == false then
+			table.insert(tableofchildren,#tableofchildren+1,value.Name)
+		end
+	end
+	if newline == true then
+		return table.concat(tableofchildren,"\n")
+	elseif newline == false then
+		return table.concat(tableofchildren,", ")
+	end
+end
+
+return HandyFunctions
